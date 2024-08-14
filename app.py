b1f084f29f0fc5e00f82887b8f3246e0599b826c
BreakingBud <68956272+BreakingBud@users.noreply.github.com>
2024-08-13 20:51:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import zipfile
-import os
 
 # Set up the app configuration
 st.set_page_config(
@@ -15,27 +13,12 @@ pages = {
     "Introduction": "introduction",
     "Head-to-Head Analysis": "head_to_head",
     "Player-to-Player Analysis": "player_analysis",
-    "Choropleth Map": "choropleth_map",
-    "World Cup 2026 Prediction": "predict_wc"
+    "Choropleth Map": "choropleth_map"
 }
 
 # Radio buttons for page selection
 selected_page = st.sidebar.radio("Go to", list(pages.keys()))
 
-# File uploader for the user to upload the zip file
-uploaded_file = st.sidebar.file_uploader("Upload your data.zip file", type="zip")
-
-if uploaded_file is not None:
-    # Path to extract the zip file
-    extracted_path = 'extracted_data'
-
-    # Extract the uploaded zip file
-    with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
-        zip_ref.extractall(extracted_path)
-
-    # Load the selected page as a module
-    page = pages[selected_page]
-    exec(open(f"{page}.py").read())
-
-else:
-    st.sidebar.warning("Please upload the data.zip file to proceed.")
+# Load the selected page as a module
+page = pages[selected_page]
+exec(open(f"{page}.py").read())
