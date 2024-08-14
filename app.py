3e7e1c865eeac4063b776a0af561bea1d94413a2
BreakingBud <68956272+BreakingBud@users.noreply.github.com>
2024-08-13 22:33:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-# Page mappings
+# Define available pages and their module names
 pages = {
     "Introduction": "introduction",
     "Head-to-Head Analysis": "head_to_head",
@@ -16,22 +16,13 @@ pages = {
     "Choropleth Map": "choropleth_map"
 }
 
-def load_page(page_name):
-    try:
-        module = importlib.import_module(pages[page_name])
-        return module.show_page
-    except ModuleNotFoundError:
-        st.error(f"Module for page '{page_name}' not found.")
-        return None
-
 def main():
-    st.sidebar.title("Navigation")
+    st.sidebar.title("Football Analysis App")
     selected_page = st.sidebar.radio("Choose a page", list(pages.keys()))
 
     # Load and show the selected page
-    page_function = load_page(selected_page)
-    if page_function:
-        page_function()
+    module = importlib.import_module(pages[selected_page])
+    module.show_page()
 
 if __name__ == "__main__":
     main()
