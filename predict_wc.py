4ad63e0b239e05b047e7589009cd94f9745e468f
BreakingBud <68956272+BreakingBud@users.noreply.github.com>
2024-08-13 20:46:52 -0400

    Update predict_wc.py

--- predict_wc.py
+++ predict_wc.py
@@ -1,99 +1,90 @@
 import streamlit as st
 import plotly.graph_objects as go
-from preprocess import load_and_preprocess_data
-from model import train_model, predict_match
 
-def create_knockout_bracket(teams, winners=None):
+def create_knockout_bracket(teams):
     st.title("FIFA World Cup Knockout Stage Bracket")
-
-    if winners is None:
-        winners = [""] * 8  # Empty placeholders for winners
 
     fig = go.Figure()
 
     # Round of 16
-    fig.add_trace(go.Scatter(
-        x=[1, 1], y=[1, 2],
-        mode="text",
-        text=[teams[0], teams[1]],
-        textposition="middle center",
-        showlegend=False
-    ))
-    fig.add_trace(go.Scatter(
-        x=[1, 1], y=[3, 4],
-        mode="text",
-        text=[teams[2], teams[3]],
-        textposition="middle center",
-        showlegend=False
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=[1, 1], y=[5, 6],
-        mode="text",
-        text=[teams[4], teams[5]],
-        textposition="middle center",
-        showlegend=False
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=[1, 1], y=[7, 8],
-        mode="text",
-        text=[teams[6], teams[7]],
-        textposition="middle center",
-        showlegend=False
-    ))
+    for i in range(8):
+        fig.add_trace(go.Scatter(
+            x=[1, 2],
+            y=[8-i, 8-i],
+            mode="lines+text",
+            text=[teams[i*2], teams[i*2+1]],
+            textposition="top center"
+        ))
+        fig.add_trace(go.Scatter(
+            x=[2],
+            y=[8-i],
+            mode="markers+text",
+            text=["Winner {}".format(i+1)],
+            textposition="middle right"
+        ))
 
     # Quarterfinals
-    fig.add_trace(go.Scatter(
-        x=[2, 2], y=[1.5, 3.5],
-        mode="text",
-        text=[winners[0], winners[1]],
-        textposition="middle center",
-        showlegend=False
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=[2, 2], y=[5.5, 7.5],
-        mode="text",
-        text=[winners[2], winners[3]],
-        textposition="middle center",
-        showlegend=False
-    ))
+    for i in range(4):
+        fig.add_trace(go.Scatter(
+            x=[3, 4],
+            y=[7-i*2, 7-i*2],
+            mode="lines+text",
+            text=["Winner {}".format(i*2+1), "Winner {}".format(i*2+2)],
+            textposition="top center"
+        ))
+        fig.add_trace(go.Scatter(
+            x=[4],
+            y=[7-i*2],
+            mode="markers+text",
+            text=["QF Winner {}".format(i+1)],
+            textposition="middle right"
+        ))
 
     # Semifinals
-    fig.add_trace(go.Scatter(
-        x=[3, 3], y=[2.5, 6.5],
-        mode="text",
-        text=[winners[4], winners[5]],
-        textposition="middle center",
-        showlegend=False
-    ))
+    for i in range(2):
+        fig.add_trace(go.Scatter(
+            x=[5, 6],
+            y=[5-i*4, 5-i*4],
+            mode="lines+text",
+            text=["QF Winner {}".format(i*2+1), "QF Winner {}".format(i*2+2)],
+            textposition="top center"
+        ))
+        fig.add_trace(go.Scatter(
+            x=[6],
+            y=[5-i*4],
+            mode="markers+text",
+            text=["SF Winner {}".format(i+1)],
+            textposition="middle right"
+        ))
 
     # Final
     fig.add_trace(go.Scatter(
-        x=[4], y=[4.5],
-        mode="text",
-        text=[winners[6]],
-        textposition="middle center",
-        showlegend=False
+        x=[7, 8],
+        y=[4, 4],
+        mode="lines+text",
+        text=["SF Winner 1", "SF Winner 2"],
+        textposition="top center"
+    ))
+    fig.add_trace(go.Scatter(
+        x=[8],
+        y=[4],
+        mode="markers+text",
+        text=["Champion"],
+        textposition="middle right"
     ))
 
     fig.update_layout(
-        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        height=600,
-        width=800,
-        showlegend=False
+        showlegend=False,
+        xaxis=dict(showticklabels=False),
+        yaxis=dict(showticklabels=False),
+        title="Knockout Stage Bracket"
     )
 
     st.plotly_chart(fig)
 
-# Load preprocessed data and model
+# Assuming you have already preprocessed data and trained a model
 try:
-    X_train, X_test, y_train, y_test, label_encoders, scaler, home_team_encoder = load_and_preprocess_data('data.zip', 'extracted_path')
-    model = train_model(X_train, y_train, X_train.shape[1])
-
-    # Placeholder teams for the bracket
+    # Dummy team names for placeholder purposes
     teams = [
         "Group A Winner", "Group B Runner-Up",
         "Group C Winner", "Group D Runner-Up",
@@ -105,11 +96,7 @@ try:
         "Group H Winner", "Group G Runner-Up"
     ]
 
-    # Placeholder winners for the first round
-    winners = [teams[i] for i in range(8)]  # This should be replaced with actual prediction logic
-
-    # Create the knockout bracket
-    create_knockout_bracket(teams, winners)
+    create_knockout_bracket(teams)
 
 except Exception as e:
     st.error(f"An error occurred: {e}")
