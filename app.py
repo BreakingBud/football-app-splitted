ba98d178c211a1fc115013d6ba81084b50a58301
BreakingBud <68956272+BreakingBud@users.noreply.github.com>
2024-08-13 22:44:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import importlib
+from constants import PAGE_TITLES
 
 # Set up the app configuration
 st.set_page_config(
@@ -8,20 +9,12 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-# Define available pages and their module names
-pages = {
-    "Introduction": "introduction",
-    "Head-to-Head Analysis": "head_to_head",
-    "Player-to-Player Analysis": "player_analysis",
-    "Choropleth Map": "choropleth_map"
-}
-
 def main():
     st.sidebar.title("Football Analysis App")
-    selected_page = st.sidebar.radio("Choose a page", list(pages.keys()))
+    selected_page = st.sidebar.radio("Choose a page", list(PAGE_TITLES.keys()))
 
     # Load and show the selected page
-    module = importlib.import_module(pages[selected_page])
+    module = importlib.import_module(f'{PAGE_TITLES[selected_page]}')
     module.show_page()
 
 if __name__ == "__main__":
