8f2eaf912e7f06a6d6a99aeccf8c07614de80b7b
BreakingBud <68956272+BreakingBud@users.noreply.github.com>
2024-08-13 22:58:24 -0400

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -39,5 +39,5 @@ def show_page():
     lottie_url = "https://lottie.host/89057c52-70ef-444e-952c-988c03e80a6a/KvI0AtqNdI.json"  # Use a football bouncing animation URL
     lottie_data = load_lottie_url(lottie_url)
     if lottie_data:
-        st_lottie(lottie_data, height=400, key="intro_lottie", quality="high", speed=0.8)
+        st_lottie(lottie_data, height=500, key="intro_lottie", quality="high", speed=0.8)
 
